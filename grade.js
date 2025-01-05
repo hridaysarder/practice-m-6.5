c3e1db218f1ab8b683f226f61ae19c3329ba67f6
HridaySarder <hridoyaug12@gmail.com>
2025-01-05 18:45:03 +0600

    first commit

---
+++ grade.js
@@ -0,0 +1,26 @@
+let result=30
+if(result>=80){
+    console.log("A+");
+}
+else if(result>=70){
+    console.log("A");
+}
+else if(result>=60){
+    console.log("A-");
+}
+else if(result>=50)
+{
+    console.log("B");
+}
+else if(result>=40)
+{
+    console.log("C");
+}
+else if(result>=33)
+{
+    console.log("D");
+}
+else 
+{
+    console.log("F");
+}
